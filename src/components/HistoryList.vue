ab807e9fff5a1856d37a30c14d999bc48389161f
Michael van Meerwijk <michaelvanm@gmail.com>
2018-12-21 17:29:35 +0100

    Tag component

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -6,7 +6,7 @@
         <v-list three-line>
 			<v-subheader>Meest recent</v-subheader>
 			<v-divider/>
-			<v-list-tile @click="" ripple>
+			<v-list-tile @click="selectTile" ripple>
 				<v-list-tile-content>
 					<v-list-tile-title>Laatste keer</v-list-tile-title>
 					<v-list-tile-sub-title>Subtitle meest recent</v-list-tile-sub-title>
@@ -15,13 +15,12 @@
 			<v-subheader>Eerder</v-subheader>
 			<v-divider/>
 			<template v-for="n in 50">
-				<v-list-tile :key="n" @click="" ripple>
+				<v-list-tile :key="n" @click="selectTile" ripple>
 					<v-list-tile-content>
 						<v-list-tile-title>Title</v-list-tile-title>
-						<v-list-tile-sub-title>Subtitle</v-list-tile-sub-title>
+						<tag>Tag test</tag>
 					</v-list-tile-content>
 				</v-list-tile>
-				<!-- <v-divider :key="`d-${n}`"/> -->
 			</template>
         </v-list>
       </v-flex>
@@ -29,9 +28,18 @@
 </template>
 
 <script>
-  export default {
-   
-  }
+import Tag from './Tag.vue';
+
+export default {
+	components: {
+		Tag
+	},
+	methods: {
+		selectTile(e) {
+
+		}
+	}
+}
 </script>
 
 <style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-chip 
+		:small="!editable"
+		:close="editable"
+		:outline="!isColor"
+		:disabled="!editable"
+		:color="isColor ? 'primary' : null"
+		:text-color="isColor ? 'white' : null"
+	>
+		<slot/>
+	</v-chip>
+</template>
+
+<script>
+export default {
+	props: {
+		editable: {
+			type: Boolean,
+			default: false
+		}
+	},
+	computed: {
+		isColor() {
+			return false;
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
